83c35405489257d14f7a82502d1437cd3730840c
Ilona-Tkachenko <ilonatkachenko796@gmail.com>
2025-03-07 15:23:33 +0200

    lab_13_done

---
+++ react-third-lab/src/App.jsx
@@ -0,0 +1,13 @@
+import "./UserForm.css";
+import UserForm from "./UserForm";
+
+function App() {
+  return (
+    <div>
+      <h1>Ласкаво просимо!</h1>
+      <UserForm />
+    </div>
+  );
+}
+
+export default App;
